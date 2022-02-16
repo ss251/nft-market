9a42737d7143740125e995f520513345a359d8b6
ss251 <ss251@uw.edu>
2022-02-16 02:00:58 -0600

    fix ethers unit utils typo

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
       data.map(async (i) => {
         const tokenUri = await tokenContract.tokenURI(i.tokenId);
         const meta = await axios.get(tokenUri);
-        let price = ethers.utils.formatUtils(i.price.toString(), "ether");
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
         let item = {
           price,
           tokenId: i.tokenId.toNumber(),
